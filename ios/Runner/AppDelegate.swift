2ef99558af003c7865643bdfb1d668b60d3e2431
Umar Ghaffar <umar.ghaffar@guitarcenter.com>
2023-01-28 02:35:34 +0500

    change key of google map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAIx_eL01XVZ07Ld1Ad09orDmV3httbrgo")
+    GMSServices.provideAPIKey("AIzaSyCR2Kkq0xSfZLvoM0LBoSgp1XRPlOg17SQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/google_map/string_constants.dart
+++ lib/google_map/string_constants.dart
@@ -1,5 +1,5 @@
 const defaultLat = '51.50880800';
 const defaultLon = '-0.69987000';
-const mapApiKey = "AIzaSyAIx_eL01XVZ07Ld1Ad09orDmV3httbrgo";
+const mapApiKey = "AIzaSyCR2Kkq0xSfZLvoM0LBoSgp1XRPlOg17SQ";
 const String myLatitude = 'myLatitude';
 const String myLongitude = 'myLongitude';
