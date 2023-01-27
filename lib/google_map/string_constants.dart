ce92649f70133254ea2a1f37a1b878dfda686e23
Umar Ghaffar <umar.ghaffar@guitarcenter.com>
2023-01-28 02:27:53 +0500

    change key of google map

--- lib/google_map/string_constants.dart
+++ lib/google_map/string_constants.dart
@@ -1,5 +1,5 @@
 const defaultLat = '51.50880800';
 const defaultLon = '-0.69987000';
-const mapApiKey = "AIzaSyCR2Kkq0xSfZLvoM0LBoSgp1XRPlOg17SQ";
+const mapApiKey = "AIzaSyAIx_eL01XVZ07Ld1Ad09orDmV3httbrgo";
 const String myLatitude = 'myLatitude';
 const String myLongitude = 'myLongitude';
