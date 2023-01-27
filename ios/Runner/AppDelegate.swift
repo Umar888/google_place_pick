9e0f2fbc151319dbe72fb22603762836e8407beb
Umar Ghaffar <umar.ghaffar@guitarcenter.com>
2023-01-28 02:55:16 +0500

    remove the keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCR2Kkq0xSfZLvoM0LBoSgp1XRPlOg17SQ")
+    GMSServices.provideAPIKey("GOOGLE_MAP_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
